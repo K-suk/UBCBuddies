2158312a05f92df0d91a04fb02afda71d622f17e
K-suk <kuma.kou.0201@gmail.com>
2024-10-03 13:12:30 -0700

    update admin settings

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,14 +7,14 @@ User = get_user_model()
 
 class UserAdmin(BaseUserAdmin):
     # 表示するフィールドのセットアップ
-    list_display = ('email', 'name', 'get_review_average', 'review_count', 'is_staff', 'is_active', 'created_at')
+    list_display = ('email', 'name', 'get_review_average', 'review_count', 'is_staff', 'is_active', 'created_at', 'profile_image')
     list_filter = ('is_staff', 'is_active', 'sex')
     search_fields = ('email', 'name')
     ordering = ('email',)
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('name', 'age', 'sex', 'contact_address', 'bio', 'review_count', 'review_sum')}),
+        (_('Personal info'), {'fields': ('name', 'age', 'sex', 'contact_address', 'bio', 'review_count', 'review_sum', 'profile_image')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
         (_('Important dates'), {'fields': ('last_login', 'created_at', 'updated_at')}),
         (_('Matching info'), {'fields': ('cur_matching', 'matching_history', 'wait', 'done', 'semi_comp')}),
